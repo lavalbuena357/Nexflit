5921092a7bd035323524ec691ef75b6ff2878fde
albertvr357 <cuervo357@gmail.com>
2021-07-16 21:54:08 -0500

    section 1 component

---
+++ src/components/Section1/Section1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './Section1.module.css';
+
+function Section1() {
+  return (
+    <div className={style.container}>
+      <div className={style.gradient}>
+        <div className={style.text_home}>
+          <h2>Películas y series ilimitadas</h2>
+          <h2>y mucho más.</h2>
+          <p>Disfruta donde quieras. Cancela cuando quieras.</p>
+          <small>Quieres ver Nexflit ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Nexflit.</small>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section1;
