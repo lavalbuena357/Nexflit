cd8bbb22665f9bb23f625c11236ad3750b3536fb
albertvr357 <cuervo357@gmail.com>
2021-07-17 02:03:13 -0500

    responsive desing

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -9,10 +9,9 @@ function Section1() {
       <div className={style.gradient}>
         <Header />
         <div className={style.text_home}>
-          <h2>Películas y series ilimitadas</h2>
-          <h2>y mucho más.</h2>
+          <h2>Películas y series ilimitadas y mucho más.</h2>
           <p>Disfruta donde quieras. Cancela cuando quieras.</p>
-          <small>Quieres ver Nexflit ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Nexflit.</small>
+          <small>¿Quieres ver Nexflit ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Nexflit.</small>
         </div>
         <Search />
       </div>
